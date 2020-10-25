710727764f00ae3657b88d2e8b7669008312a4a3
theobori <theo1.bori@epitech.eu>
2020-10-25 23:25:29 +0100

    [+] colors

--- wall.sh
+++ wall.sh
@@ -1,24 +1,33 @@
+colors=("-r" "-g" "-y" "-b" "-p" "-c" "--rainbow")
+
 fancy_clean () {
     printf "\x1b[H"
     for cursor in $(seq 1 $((($1*$2)/$3))); do
         for size in $(seq 1 $(($3))); do
             printf "\x20"
         done
+        sleep 0.005
     done
 }
 
-error_handling () {
-    if [[ -z $1 || -z $2 || $1 != "-s" ]]; then
-        printf "syntax : $0 -s <brick size>\n"
-        exit
-    fi
+display_error () { # TODO: help message
+    printf "syntax : $0 -s <brick size>\n"
+    exit
 }
 
-main () {
-    clear
+error_handling () {
+    error=True
+    if [[ -z $1 || -z $2 ]]; then display_error; fi
+    for x in ${colors[*]}; do
+        if [ $1 == $x ]; then error=False; fi
+    done
+    if [ $error = True ]; then display_error; fi
+}
+
+rainbow () {
     while :; do
         le=$1;co=$(tput cols);li=$(tput lines);f=0;r=$((RANDOM % 6 + 31))
-        for y in $(seq 1 $(((co*li)/le))); do
+        for y in $(seq 1 $(((co*li)/le+1))); do
             c=$((RANDOM % 6 + 31))
             if [ $((c)) == $((r)) ]; then f=5; fi
             for x in $(seq 1 $((le))); do
@@ -29,5 +38,27 @@ main () {
         fancy_clean $co $li $le
     done
 }
+
+uni_color () {
+    while :; do
+        le=$1;co=$(tput cols);li=$(tput lines)
+        for y in $(seq 1 $(((co*li)/le+1))); do
+            for x in $(seq 1 $((le))); do
+                printf "\e[%dm\xe2\x96\x88\e[0m" $1
+            done
+            sleep 0.05
+        done
+        fancy_clean $co $li $le
+    done
+}
+
+main () {
+    clear
+    for i in {0..6}; do
+        if [ $1 == ${colors[$i]} ]; then uni_color $((31+$i)); fi
+    done
+    rainbow $2
+}
+
 error_handling $1 $2
-main $2
+main $1 $2
